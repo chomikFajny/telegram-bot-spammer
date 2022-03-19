88aff588b2e25c438d7bf28b90098cff562e6b36
chomik <73186053+chomikFajny@users.noreply.github.com>
2022-03-19 18:24:23 +0100

    🐹 mmm spammer ⭐

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 token = input("token of bot: ")
 id = input("chat id : ")
 text = input("text to send : ")
-while true:
+while True:
     requests.post("https://api.telegram.org/bot"{token}"/sendMessage?chat_id="{id}"&text="{text})
     print("Message send :D " + text)
     
